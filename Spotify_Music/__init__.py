5c8b124a82998bb85aa1f6688c8884ae38957cac
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-08-09 16:17:14 +0530

    Create __init__.py

---
+++ Spotify_Music/__init__.py
@@ -0,0 +1,31 @@
+from Spotify_Music.core.bot import Spotify
+from Spotify_Music.core.dir import dirr
+from Spotify_Music.core.git import git
+from Spotify_Music.core.userbot import Userbot
+from Spotify_Music.misc import dbb, heroku
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Spotify()
+userbot = Userbot()
+api = SafoneAPI()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+YTB = YTM()
+
+APP = "Gaana_MusicBot"  # connect music api key "Dont change it"
