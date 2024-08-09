1dae85c071bd9a7b74f76d2d1351aa71a48a3292
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-08-09 16:24:41 +0530

    Update __main__.py

--- Spotify_Music/__main__.py
+++ Spotify_Music/__main__.py
@@ -5,14 +5,14 @@ from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from Spotify import LOGGER, app, userbot
-from Spotify.core.call import Spotify
-from Spotify.misc import sudo
-from Spotify.plugins import ALL_MODULES
-from Spotify.utils.database import get_banned_users, get_gbanned
+from Spotify_Music import LOGGER, app, userbot
+from Spotify_Music.core.call import Spotify
+from Spotify_Music.misc import sudo
+from Spotify_Music.plugins import ALL_MODULES
+from Spotify_Music.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
-from Spotify.plugins.tools.clone import restart_bots
+from Spotify_Music.plugins.tools.clone import restart_bots
 
 
 
@@ -39,28 +39,29 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("Spotify.plugins" + all_module)
-    LOGGER("Spotify.plugins").info("Successfully Imported Modules...")
+        importlib.import_module("Spotify_Music.plugins" + all_module)
+    LOGGER("Spotify_Music.plugins").info("Successfully Imported Modules...")
     await userbot.start()
     await Spotify.start()
     try:
-        await Spotify.stream_call("https://telegra.ph/file/7f3de9e11bca6274101ea.mp4")
+        await Spotify.stream_call("https://graph.org/file/e999c40cb700e7c684b75.mp4")
     except NoActiveGroupCall:
-        LOGGER("Spotify").error(
+        LOGGER("Spotify_Music").error(
             "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
         )
         exit()
     except:
         pass
     await Spotify.decorators()
-    LOGGER("Spotify").info(
+    LOGGER("Spotify_Music").info(
         "╔═════ஜ۩۞۩ஜ════╗\n  🌸ᴍᴀᴅᴇ ʙʏᴇ ᴛᴇᴀᴍ ᴘʙx (sᴜᴋʜ) 🌸\n╚═════ஜ۩۞۩ஜ════╝"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("Spotify").info("Stopping Brandrd Music Bot...")
+    LOGGER("Spotify_Music").info("Stopping Brandrd Music Bot...")
 
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(init())
+    
