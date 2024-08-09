53160ffe0427d8ad9f4679af73d654ebbf81a5f3
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-08-09 23:53:16 +0530

    Update and rename uodate.py to update.py

--- Spotify_Music/plugins/BAD(PB05)/update.py
+++ Spotify_Music/plugins/BAD(PB05)/update.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from BrandrdXMusic import app
+from Spotify_Music import app
 from pyrogram import filters
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
