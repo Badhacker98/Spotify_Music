4480125659b3cc2d8b85ce0de6081cb0b3e9426e
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-08-09 19:00:50 +0530

    Create logger.py

---
+++ Spotify_Music/plugins/BAD(PB05)/logger.py
@@ -0,0 +1,24 @@
+from pyrogram import filters
+
+from Spotify_Music import app
+from Spotify_Music.misc import SUDOERS
+from Spotify_Music.utils.database import add_off, add_on
+from Spotify_Music.utils.decorators.language import language
+
+
+@app.on_message(filters.command(["logger"]) & SUDOERS)
+@language
+async def logger(client, message, _):
+    usage = _["log_1"]
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    state = message.text.split(None, 1)[1].strip().lower()
+    if state == "enable":
+        await add_on(2)
+        await message.reply_text(_["log_2"])
+    elif state == "disable":
+        await add_off(2)
+        await message.reply_text(_["log_3"])
+    else:
+        await message.reply_text(usage)
+      
