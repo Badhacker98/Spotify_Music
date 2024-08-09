f2ff024d26dd2f70555f0af7cecc3a9d61029c02
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-08-09 21:55:23 +0530

    Update setting.py

--- Spotify_Music/plugins/BAD(PB05)/setting.py
+++ Spotify_Music/plugins/BAD(PB05)/setting.py
@@ -54,7 +54,7 @@ async def settings_mar(client, message: Message, _):
 @languageCB
 async def gib_repo(client, CallbackQuery, _):
     await CallbackQuery.edit_message_media(
-        InputMediaVideo("https://te.legra.ph/file/ef47f077b671f69f8d8f0.mp4"),
+        InputMediaVideo("https://telegra.ph/file/bda2c51bd00c8f4710b04.mp4"),
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
         ),
