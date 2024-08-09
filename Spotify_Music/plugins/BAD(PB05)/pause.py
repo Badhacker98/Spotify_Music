48be958566fae6380a8b44318a2a20b09aa1822b
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-08-09 17:59:44 +0530

    Update pause.py

--- Spotify_Music/plugins/BAD(PB05)/pause.py
+++ Spotify_Music/plugins/BAD(PB05)/pause.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from Spotify_Music import app
-from Spotify_Music.core.call import Hotty
+from Spotify_Music.core.call import Spotify
 from Spotify_Music.utils.database import is_music_playing, music_off
 from Spotify_Music.utils.decorators import AdminRightsCheck
 from Spotify_Music.utils.inline import close_markup
@@ -15,7 +15,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Hotty.pause_stream(chat_id)
+    await Spotify.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
