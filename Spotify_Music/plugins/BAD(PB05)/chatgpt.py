143d3b9d64f74099f8a09ca606b0a2125a547c52
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-08-10 14:45:07 +0530

    Create chatgpt.py

---
+++ Spotify_Music/plugins/BAD(PB05)/chatgpt.py
@@ -0,0 +1,31 @@
+import os
+import random
+import time
+from Spotify_Music import app
+import requests
+from pyrogram.types import  Message
+from pyrogram.types import InputMediaPhoto
+from BadAPI import api
+from pyrogram.enums import ChatAction, ParseMode
+from pyrogram import filters
+
+
+@app.on_message(
+    filters.command(
+        ["chatgpt", "ai", "ask", "gpt", "solve"],
+        prefixes=["+", ".", "/", "-", "", "$", "#", "&"],
+    )
+)
+async def chat_gpt(bot, message):
+    
+    try:
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply_text(
+            "Example:**\n\n/chatgpt Where is golden temple?")
+        else:
+            a = message.text.split(' ', 1)[1]
+            r=api.gemini(a)["results"]
+            await message.reply_text(f" {r} \n\n🌸 ᴘᴏᴡᴇʀᴇᴅ @PBX_PERMOT", parse_mode=ParseMode.MARKDOWN)     
+    except Exception as e:
+        await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")
