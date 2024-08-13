51bb14b09c41010db62c0aeb60f2647f1a0d4f16
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-08-13 19:04:21 +0530

    Update call.py

--- Spotify_Music/core/call.py
+++ Spotify_Music/core/call.py
@@ -20,7 +20,7 @@ from pytgcalls.types import (
 from pytgcalls.types.stream import StreamAudioEnded
 
 import config
-from Spotify_Music import LOGGER, YouTube, app, YTB
+from Spotify_Music import LOGGER, YouTube, app
 from Spotify_Music.misc import db
 from Spotify_Music.utils.database import (
     add_active_chat,
@@ -442,15 +442,7 @@ class Call(PyTgCalls):
                         video=str(streamtype) == "video",
                     )
                 except:
-                    try:
-                        file_path, direct = await YTB.download(
-                            videoid,
-                            mystic,
-                            videoid=True,
-                            video=str(streamtype) == "video",
-                        )
-                    except:
-                        return await mystic.edit_text(
+                    return await mystic.edit_text(
                         _["call_6"], disable_web_page_preview=True
                     )
                 if video:
