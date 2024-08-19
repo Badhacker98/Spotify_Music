f96cbc2977d0fd66d65aab0d099a7c55943d1577
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-08-19 12:49:26 +0530

    Update scanner.py

--- Spotify_Music/plugins/BAD(PB05)/scanner.py
+++ Spotify_Music/plugins/BAD(PB05)/scanner.py
@@ -1,3 +1,11 @@
+import asyncio
+
+from Spotify_Music import app
+from pyrogram import filters
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from config import MUSIC_BOT_NAME
+
 @app.on_message(filters.command(["donate, qr, scanner"]))
 async def start(client: Client, message: Message):
     await message.reply_video(
