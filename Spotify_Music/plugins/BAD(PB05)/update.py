c7d18662af43d2a5a43b5e8f2b0b60022d4ce25b
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-08-19 12:44:46 +0530

    Update update.py

--- Spotify_Music/plugins/BAD(PB05)/update.py
+++ Spotify_Music/plugins/BAD(PB05)/update.py
@@ -30,3 +30,10 @@ async def start(client: Client, message: Message):
         )
     )
   
+# donate #
+@app.on_message(filters.command(["donate, qr, scanner"]))
+async def start(client: Client, message: Message):
+    await message.reply_video(
+        video=f"https://te.legra.ph/file/7fa3be2dbc0e3d63d2c0d.jpg",
+        caption=f"❤️ ʜᴇʏ {message.from_user.mention}",
+    )
