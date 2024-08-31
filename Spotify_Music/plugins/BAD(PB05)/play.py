5cbd4cf8bad06bb7fb777db75e54585cffcbd1c3
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-08-31 18:56:07 +0530

    Update play.py

--- Spotify_Music/plugins/BAD(PB05)/play.py
+++ Spotify_Music/plugins/BAD(PB05)/play.py
@@ -54,21 +54,6 @@ async def play_commnd(
     url,
     fplay,
 ):
-    if not await is_served_user(message.from_user.id):
-        await message.reply_text(
-            text="Error, You're Not A Verified User ❌\nPlease Click On The Below Button To Verify Yourself .",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            text="Click For Play Or Verify Here",
-                            url=f"https://t.me/{app.username}?start=verify",
-                        )
-                    ]
-                ]
-            ),
-        )
-        return
     mystic = await message.reply_text(
         _["play_2"].format(channel) if channel else _["play_1"]
     )
