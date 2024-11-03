76d59b62e678b280876c1a5ab56cbd7e3c5d8b99
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-03 22:16:28 +0530

    Update thumbnails.py

--- Spotify_Music/utils/thumbnails.py
+++ Spotify_Music/utils/thumbnails.py
@@ -160,4 +160,25 @@ async def get_thumb(videoid):
     background.save(f"cache/{videoid}_v4.png")
     return f"cache/{videoid}_v4.png"
 
-    
+
+async def gen_thumb(videoid):
+    try:
+        query = f"https://www.youtube.com/watch?v={videoid}"
+        results = VideosSearch(query, limit=1)
+        for result in (await results.next())["result"]:
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+        return thumbnail
+    except Exception as e:
+        return f"https://img.youtube.com/vi/{videoid}/maxresdefault.jpg"
+
+
+async def gen_qthumb(vidid):
+    try:
+        query = f"https://www.youtube.com/watch?v={vidid}"
+        results = VideosSearch(query, limit=1)
+        for result in (await results.next())["result"]:
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+        return thumbnail
+    except Exception as e:
+        return f"https://img.youtube.com/vi/{videoid}/maxresdefault.jpg"
+        
