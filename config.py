abafa252587bd43ee26888ddd28a2e1a7c3db09a
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-03 23:13:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -81,7 +81,6 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 
 
 # Get your pyrogram v2 session
-STRING_SESSIONS = list(map(str.strip, getenv("STRING_SESSIONS", None)))
 STRING1 = getenv("STRING_SESSION", None)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
