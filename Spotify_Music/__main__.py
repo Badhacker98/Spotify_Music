0a23bc77515442e4cc63f8e0c03d2974990b9c95
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-03 22:30:31 +0530

    Update __main__.py

--- Spotify_Music/__main__.py
+++ Spotify_Music/__main__.py
@@ -1,32 +1,26 @@
-import asyncio
 import importlib
-from sys import argv
+
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from Spotify_Music import LOGGER, app, userbot
-from Spotify_Music.core.call import Spotify
-from Spotify_Music.misc import sudo
+from config import BANNED_USERS
+from Spotify_Music import HELPABLE, LOGGER, app, userbot
+from Spotify_Music.core.call import Yukki
 from Spotify_Music.plugins import ALL_MODULES
 from Spotify_Music.utils.database import get_banned_users, get_gbanned
-from config import BANNED_USERS
-
-
-
 
 
 async def init():
-    if (
-        not config.STRING1
-        and not config.STRING2
-        and not config.STRING3
-        and not config.STRING4
-        and not config.STRING5
-    ):
-        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
-        exit()
-    await sudo()
+    if len(config.STRING_SESSIONS) == 0:
+        LOGGER("Spotify_Music").error(
+            "No Assistant Clients Vars Defined!.. Exiting Process."
+        )
+        return
+    if not config.SPOTIFY_CLIENT_ID and not config.SPOTIFY_CLIENT_SECRET:
+        LOGGER("Spotify_Music").warning(
+            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
+        )
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -34,33 +28,37 @@ async def init():
         users = await get_banned_users()
         for user_id in users:
             BANNED_USERS.add(user_id)
-    except:
+    except Exception:
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("Spotify_Music.plugins" + all_module)
-    LOGGER("Spotify_Music.plugins").info("Successfully Imported Modules...")
+        imported_module = importlib.import_module(all_module)
+
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                HELPABLE[imported_module.__MODULE__.lower()] = imported_module
+    LOGGER("Spotify_Music.plugins").info("Successfully Imported All Modules ")
     await userbot.start()
-    await Spotify.start()
+    await Yukki.start()
+    LOGGER("Spotify_Music").info("Assistant Started Sucessfully")
     try:
-        await Spotify.stream_call("https://graph.org/file/e999c40cb700e7c684b75.mp4")
+        await Yukki.stream_call(
+            "https://graph.org/file/e999c40cb700e7c684b75.mp4"
+        )
     except NoActiveGroupCall:
         LOGGER("Spotify_Music").error(
             "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
         )
-        exit()
-    except:
-        pass
-    await Spotify.decorators()
-    LOGGER("Spotify_Music").info(
-        "╔═════ஜ۩۞۩ஜ════╗\n  🌸ᴍᴀᴅᴇ ʙʏᴇ ᴛᴇᴀᴍ ᴘʙx (sᴜᴋʜ) 🌸\n╚═════ஜ۩۞۩ஜ════╝"
-    )
+        sys.exit()
+
+    await Yukki.decorators()
+    LOGGER("Spotify_Music").info("╔═════ஜ۩۞۩ஜ════╗\n  🌸ᴍᴀᴅᴇ ʙʏᴇ ᴛᴇᴀᴍ ᴘʙx (sᴜᴋʜ) 🌸\n╚═════ஜ۩۞۩ஜ════╝")
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("Spotify_Music").info("Stopping Brandrd Music Bot...")
 
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(init())
-    
+    app.run(init())
+    LOGGER("Spotify_Music").info("Stopping Spotify_Music! GoodBye")
+        
