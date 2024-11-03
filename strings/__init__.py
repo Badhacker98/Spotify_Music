19af5dc6d3f750bb0ccf19debbac098d9c58270e
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-03 22:39:58 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,105 +1,17 @@
 import os
 from typing import List
-from Spotify_Music.utils.database import get_lang
 
 import yaml
 
 LOGGERS = "Gaana_MusicBot"
 
-
 languages = {}
-commands = {}
-helpers = {}
 languages_present = {}
-
-
-def load_yaml_file(file_path: str) -> dict:
-    with open(file_path, "r", encoding="utf8") as file:
-        return yaml.safe_load(file)
-
-
-def get_command(lang: str = "en") -> Union[str, List[str]]:
-    if lang not in commands:
-        lang = "en"
-    return commands[lang]
 
 
 def get_string(lang: str):
     return languages[lang]
 
-
-def get_helpers(lang: str):
-    return helpers[lang]
-    
-
-# Load English commands first and set English keys
-commands["en"] = load_yaml_file(r"./strings/cmds/en.yml")
-english_keys = set(commands["en"].keys())
-
-for filename in os.listdir(r"./strings/cmds/"):
-    if filename.endswith(".yml") and filename != "en.yml":
-        language_code = filename[:-4]
-        commands[language_code] = load_yaml_file(
-            os.path.join(r"./strings/cmds/", filename)
-        )
-
-        missing_keys = english_keys - set(commands[language_code].keys())
-        if missing_keys:
-            print(
-                f"Error: Missing keys in strings/cmds/{language_code}.yml: {', '.join(missing_keys)}"
-            )
-            sys.exit(1)
-
-for filename in os.listdir(r"./strings/helpers/"):
-    if filename.endswith(".yml"):
-        language_code = filename[:-4]
-        helpers[language_code] = load_yaml_file(
-            os.path.join(r"./strings/helpers/", filename)
-        )
-
-if "en" not in languages:
-    languages["en"] = load_yaml_file(r"./strings/langs/en.yml")
-    languages_present["en"] = languages["en"]["name"]
-
-for filename in os.listdir(r"./strings/langs/"):
-    if filename.endswith(".yml") and filename != "en.yml":
-        language_name = filename[:-4]
-        languages[language_name] = load_yaml_file(
-            os.path.join(r"./strings/langs/", filename)
-        )
-
-        for item in languages["en"]:
-            if item not in languages[language_name]:
-                languages[language_name][item] = languages["en"][item]
-
-        try:
-            languages_present[language_name] = languages[language_name]["name"]
-        except KeyError:
-            print(
-                "There is an issue with the language file. Please report it to TheTeamvk at @TheTeamvk on Telegram"
-            )
-            sys.exit()
-
-if not commands:
-    print(
-        "There's a problem loading the command files. Please report it to TheTeamVivek at @TheTeamVivek on Telegram"
-    )
-    sys.exit()
-
-
-def command(
-    commands: Union[str, List[str]],
-    prefixes: Union[str, List[str], None] = "/",
-    case_sensitive: bool = False,
-):
-    async def func(flt, client: Client, message: Message):
-        lang_code = await get_lang(message.chat.id)
-
-        if isinstance(commands, str):
-            commands_list = [commands]
-        else:
-            commands_list = commands
-            
 
 for filename in os.listdir(r"./strings/langs/"):
     if "en" not in languages:
